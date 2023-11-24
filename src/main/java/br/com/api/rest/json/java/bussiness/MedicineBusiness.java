09edb193d269a5f86250e58e3467949dfd0b5863
Felipe <felipe.francc2@gmail.com>
2023-11-24 16:19:20 -0300

    Remove unnecessary comments in MedicineBusiness class

--- src/main/java/br/com/api/rest/json/java/bussiness/MedicineBusiness.java
+++ src/main/java/br/com/api/rest/json/java/bussiness/MedicineBusiness.java
@@ -6,12 +6,6 @@ import br.com.api.rest.json.java.dao.MedicineDAO;
 import br.com.api.rest.json.java.interfaces.Business;
 import br.com.api.rest.json.java.model.Medicine;
 
-/**
- * 
- * @author Diovani Bernardi da Motta Data: 15/04/2016 Classe respons�vel por
- *         efetuar o encapsulamento da regra de neg�cio associada a entidade
- *         persitente
- */
 public class MedicineBusiness implements Business<Medicine> {
 
 	private MedicineDAO medicineDAO = new MedicineDAO();
